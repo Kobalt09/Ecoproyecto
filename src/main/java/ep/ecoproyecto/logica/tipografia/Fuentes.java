c5859fb4fbe411490aa2bacebebf0dde70a129b4
Fonso <sjalvarado08@hotmail.com>
2024-06-19 17:20:21 -0400

    fuentes y dos sprites

--- src/main/java/ep/ecoproyecto/logica/tipografia/Fuentes.java
+++ src/main/java/ep/ecoproyecto/logica/tipografia/Fuentes.java
@@ -16,7 +16,7 @@ public class Fuentes {
     Font fuente=null;
     
     public String upheaval="/fuente/upheavtt.ttf";
-
+     public String Roboto="/fuente/Roboto-Regular.ttf";
     
     public Font fuente(String nFuente, int estilo, float tamanio){
         
